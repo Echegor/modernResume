36ae65d173aa24685e095b0dc221d1157bf24142
Luis Echegorri <lme4@njit.edu>
2018-10-09 15:43:01 -0400

    Added projects Section Added new jade image

---
+++ src/app/data/project.ts
@@ -0,0 +1,6 @@
+export class Project {
+  title: string;
+  subtitle: string;
+  content: string;
+  images: Array<string>;
+}
--- src/app/portfolio/portfolio.component.ts
+++ src/app/portfolio/portfolio.component.ts
@@ -1,5 +1,6 @@
 import { Component, ViewChild, ChangeDetectorRef, AfterViewInit } from '@angular/core';
 import { NguCarousel, NguCarouselConfig } from '@ngu/carousel';
+import {Project} from "../data/project";
 
 @Component({
   selector: 'app-portfolio',
@@ -12,14 +13,46 @@ export class PortfolioComponent implements AfterViewInit {
   resetAnim = true;
   @ViewChild('myCarousel') myCarousel: NguCarousel;
   carouselConfig: NguCarouselConfig = {
-    grid: { xs: 1, sm: 1, md: 1, lg: 1, all: 0 },
+    grid: { xs: 1, sm: 2, md: 3, lg: 4, all: 0 },
     load: 3,
     interval: {timing: 4000, initialDelay: 1000},
     loop: true,
     touch: true,
     velocity: 0.2
   };
-  carouselItems = [1, 2, 3];
+  carouselItems: Array<Project> = [
+    {
+      title: 'Mobile Override Manager',
+      subtitle: 'Wakefern Food Corp.',
+      content: 'Application designed to facilitate ShopRite\'s front end eciency. The application ' +
+        'allows for a front end manager, who holds an Android application, and cashiers, who are sitting at a terminal, to ' +
+        'exchange messages as well as to request services from each other. It also allows for managers to monitor cashiers ' +
+        ', to interact with the transaction itself, to perform manager overrides ' +
+        'remotely. The mobile override manager project saved Wakefern around 40 labor hours per week per store.',
+      images: ['../../assets/images/mom/mom_login.png','../../assets/images/mom/mom_main.png','../../assets/images/mom/mom_more.png'],
+    },
+    {
+      title: 'Jade',
+      subtitle: 'Wakefern Food Corp.',
+      content: 'Web service integration into the Toshiba ACE point of sale system for a new high speed checkout ' +
+        'scanner. Built unit tests as well as used Java Visual VM to prole performance in order to ensure each ' +
+        'item being scannedtook no more than 50 ms to complete in the system.',
+      images: ['../../assets/images/jade/jade.png','../../assets/images/jade/jade2.png'],
+    },
+    {
+      title: 'Hour Tracker',
+      subtitle: 'Personal',
+      content: 'Native Android application built to track a person\'s daily activities as well as hours worked. ' +
+        'Leveraged Android design guidelines in order to create a visually appealing and simple user interface.',
+      images: ['../../assets/images/hourtracker/hourtrackermain.png'],
+    },
+    {
+      title: 'ShopRite Coupon Application:',
+      subtitle: 'Personal',
+      content: 'Built a personal application in order to make the coupon clipping process easier.',
+      images: ['../../assets/images/ShopRiteCoupons/ShopriteCoupons1.jpg','../../assets/images/ShopRiteCoupons/ShopriteCoupons2.jpg'],
+    },
+  ];
 
   constructor(private cdr: ChangeDetectorRef) {}
 
