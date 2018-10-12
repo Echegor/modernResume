ef6fa6acf64d5eabd1aaa05b1e0ec4cc7dcf5283
Luis Echegorri <lme4@njit.edu>
2018-10-12 15:41:51 -0400

    Finished general website layout

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {map} from 'rxjs/operators';
+import {Breakpoints, BreakpointState, BreakpointObserver} from '@angular/cdk/layout';
 
 @Component({
   selector: 'app-skills',
@@ -6,10 +8,94 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./skills.component.scss']
 })
 export class SkillsComponent implements OnInit {
+  // languages: string[] = [
+  //   'C++', 'C', 'Java', 'Bash', 'Batch', 'PHP', 'Python', 'JavaScript', 'SQL', 'Groovy','TypeScript'
+  // ];
+  //
+  // technologies: string[] =[
+  //   'Android', 'Spring', 'CSS', 'XML', 'HTML', 'Grails', 'JQuery', 'AngularJS', 'Hibernate', 'JDBC', 'SOAP',
+  //   'Web Services', 'Web Sockets', 'Typescript', 'NPM', 'NodeJS'
+  // ];
+  //
+  // buildTools: string[] =[
+  //   'Gradle','Maven'
+  // ];
+  //
+  // testingFrameworks: string[] = [
+  //   'Junit','Mockito'
+  // ];
+  //
+  // ide : string[] = [
+  //   'Intellij','Eclipse','RAD'
+  // ];
+  //
+  // sourceControl: string[] = [
+  //   'Git','RTC'
+  // ];
 
-  constructor() { }
+  /** Based on the screen size, switch from standard to one column per row */
+  languages: string =
+    'C++, C, Java, Bash, Batch, PHP, Python, JavaScript, SQL, Groovy, TypeScript'
+  ;
+
+  technologies: string =
+    'Android, Spring, CSS, XML, HTML, Grails, JQuery, AngularJS, Hibernate, JDBC, SOAP, Web Services, Web Sockets, NPM, NodeJS'
+  ;
+
+  buildTools: string =
+    'Gradle, Maven'
+  ;
+
+  testingFrameworks: string =
+    'Junit, Mockito'
+  ;
+
+  ide: string =
+    'Intellij, Eclipse, RAD'
+  ;
+
+  sourceControl: string =
+    'Git, RTC'
+  ;
+
+  columns: number = 3;
+  rowHeight: string = '250px';
+  cards = this.breakpointObserver.observe(Breakpoints.Handset).pipe(
+    map(({matches}) => {
+      if (matches) {
+        setTimeout(() => {
+          this.columns = 1;
+          this.rowHeight = '300px';
+        });
+
+        return [
+          {title: 'Languages', cols: 1, rows: 1, content: this.languages},
+          {title: 'Technologies', cols: 1, rows: 1, content: this.technologies},
+          {title: 'Build Tools', cols: 1, rows: 1, content: this.buildTools},
+          {title: 'Source Control', cols: 1, rows: 1, content: this.sourceControl}
+        ];
+      }
+      else{
+        setTimeout(() => {
+          this.columns = 3;
+          this.rowHeight = '250px';
+        });
+
+        return [
+          {title: 'Languages', cols: 3, rows: 1, content: this.languages},
+          {title: 'Technologies', cols: 1, rows: 1, content: this.technologies},
+          {title: 'Build Tools', cols: 1, rows: 1, content: this.buildTools},
+          {title: 'Source Control', cols: 1, rows: 1, content: this.sourceControl}
+        ];
+      }
+
+    })
+  );
+
+  constructor(private breakpointObserver: BreakpointObserver) { }
 
   ngOnInit() {
   }
+
 
 }
